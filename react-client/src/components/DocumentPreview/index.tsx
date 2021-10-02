445c51e0025fd1898bc18e723ad56dbd70b93271
Szuumii <jakub.szumskii@gmail.com>
2021-10-03 00:06:08 +0200

    sticky pdf viewer

--- react-client/src/components/DocumentPreview/index.tsx
+++ react-client/src/components/DocumentPreview/index.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import Box from '../Box';
+import Box, {Center} from '../Box';
 import { Document, Page, pdfjs } from 'react-pdf';
+import Card from './parts';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 import 'react-pdf/dist/umd/Page/AnnotationLayer.css';
+
+const options = {
+  useSystemFonts: false
+};
 
 interface Props {
   documentTitle: string;
 }
 export const DocumentPreview: React.FC<Props> = ({ documentTitle }) => {
   return (
-    <Box>
-      <Box width="600px" position="relative" margin="2rem auto">
-        <Document
-          file={{
-            url: 'http://localhost:8080/out.pdf',
-          }}
-        >
-          <Page pageNumber={1} />
-        </Document>
+    <Card>
+      <Box width="100%" height="92vh">
+        <Center>
+          <Document
+            file={{
+              url: 'http://localhost:8080/out.pdf',
+            }}
+            renderMode='svg'
+            options={options}
+          >
+            <Page pageNumber={1} />
+          </Document>
+        </Center>
       </Box>
-    </Box>
+    </Card>
   );
 };
 
---
+++ react-client/src/components/DocumentPreview/parts.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+  position: -webkit-sticky;
+  position: sticky;
+  top: -50px;
+  border: 1px solid white;
+  margin: 1rem;
+  background-color: white;
+  border-radius: 9px;
+  padding: 1.5rem;
+  box-shadow: 0 0.7rem 1.3rem rgb(0 0 0 / 4%);
+`;
+
+export default Card;
