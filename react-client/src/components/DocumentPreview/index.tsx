7459a6d4bae844d14973bc4fdf47943db0c821ab
Szuumii <jakub.szumskii@gmail.com>
2021-10-02 22:55:49 +0200

    text-layer-missalignment

--- react-client/src/components/DocumentPreview/index.tsx
+++ react-client/src/components/DocumentPreview/index.tsx
@@ -3,6 +3,8 @@ import Box from '../Box';
 import { Document, Page, pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
+import 'react-pdf/dist/umd/Page/AnnotationLayer.css';
+
 interface Props {
   documentTitle: string;
 }
