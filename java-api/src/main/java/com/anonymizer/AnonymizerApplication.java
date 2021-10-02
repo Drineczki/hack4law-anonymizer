3032196c2b4a75de990053341f55b12500ae21ff
CallMeSweets <grz3lak1997@gmail.com>
2021-10-02 15:18:54 +0200

    file upload

--- java-api/src/main/java/com/anonymizer/AnonymizerApplication.java
+++ java-api/src/main/java/com/anonymizer/AnonymizerApplication.java
@@ -2,8 +2,12 @@ package com.anonymizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class AnonymizerApplication {
 
     public static void main(String[] args) {
