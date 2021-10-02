74930cebc6ea57333ff0af7dca132baebf8222d3
Szuumii <jakub.szumskii@gmail.com>
2021-10-02 18:01:43 +0200

    python-endpoint

--- python-api/main.py
+++ python-api/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+class AnonimizeRequest(BaseModel):
+    text: str
+
+class AnonimizeResponse(BaseModel):
+    entity: str
+    anonymization: str
+    anon_type: str
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello from python!"}
 
 
-@app.get("/test-python-route")
-async def root():
+@app.get("/anonymize")
+async def root(item: AnonimizeRequest):
+    print(item.text)
     return {"message": "Python api is working!"}
